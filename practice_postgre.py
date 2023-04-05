aeb9fcf4033b454baac3ba9ad3e318099a6163da
00150 <trendyou@naver.com>
2023-04-05 11:11:25 +0900

    Onepeice_Table_in_postgre

--- practice_postgre.py
+++ practice_postgre.py
@@ -14,5 +14,33 @@ conn = psycopg2.connect(
     password="유저 비밀번호")
 """
 
+# postgtresql db와 연결하기 위한 connection 객체 생성, 이후 기본 설정 입력.
+conn = psycopg2.connect(
+    host = 'ruby.db.elephantsql.com',
+    database = 'jgsxmzrp',
+    user = 'jgsxmzrp',
+    password = 'tqFfsJhbZ6beLfIMwlgPUFeHf0dE2Ays')
 
 
+# 이후 연결된 객체에 명령을 내릴 수 있는 커서를 생성합니다.
+cur = conn.cursor()
+
+# 생성되어 있는 테이블을 지웁니다.
+cur.execute("""DROP TABLE onepiece""")
+
+# 이후 위에서 지운 테이블을 다시 생성합니다.
+cur.execute("""CREATE TABLE onepiece(Onepiece_Id INTEGER PRIMARY KEY,
+            Name VARCHAR(128) NOT NULL,
+            Position VARCHAR(128) NOT NULL)""")
+
+
+# 생성된 테이블에 데이터를 집어넣어봅시다, f-formating을 이용하여 진행합니다.
+insert = "INSERT INTO onepiece (Name, Position);" 
+query_1 = f"{insert} VALUES (Luffy, Captain)"
+query_2 = f"{insert} VALUES (Zoro, Vice Caption)"
+query_3 = f"{insert} VALUES (Nami, Mate)"
+
+# 데이터를 추가하기 위해 commit을 날려줍시다. / 이때 commit 연결된 부분에 진행합니다.
+conn.commit()
+
+print('done!')
